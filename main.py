c11194e0385e3142657d84691d47076669e270f1
paul-reiners <paul.reiners@gmail.com>
2021-08-12 10:08:15 -0500

    Removed some unnecessary requirements.

---
+++ main.py
@@ -0,0 +1,10 @@
+from lab2im.image_generator import ImageGenerator
+
+
+def gen_images():
+    brain_generator = ImageGenerator("./tutorials/data_example/brain_label_map.nii.gz")
+    im, lab = brain_generator.generate_image()
+
+
+if __name__ == '__main__':
+    gen_images()
