745b0237f74d86f2489e41979a0d264b236366f8
paul-reiners <paul.reiners@gmail.com>
2021-09-15 14:26:48 -0500

    Can specify the number of images generated.

--- lab2im/dcan/image_generation_from_folder.py
+++ lab2im/dcan/image_generation_from_folder.py
@@ -5,11 +5,11 @@ from os.path import isfile, join
 from lab2im.dcan.image_generation import generate_images
 
 
-def generate_images_from_folder(input_folder, output_folder, priors_folder):
+def generate_images_from_folder(input_folder, output_folder, priors_folder, image_count):
     only_files = [f for f in listdir(input_folder) if isfile(join(input_folder, f))]
     for file_name in only_files:
-        generate_images(join(input_folder, file_name), priors_folder, output_folder, 1)
+        generate_images(join(input_folder, file_name), priors_folder, output_folder, image_count)
 
 
 if __name__ == "__main__":
-    generate_images_from_folder(sys.argv[1], sys.argv[2], sys.argv[3])
+    generate_images_from_folder(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]))
