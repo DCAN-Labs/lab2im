f328c09d163dadb55fbb3eae95a53724cfb0c74c
paul-reiners <paul.reiners@gmail.com>
2021-09-28 15:51:27 -0500

    Generalized a script.

--- lab2im/dcan/fix_unzipped_files.py
+++ lab2im/dcan/fix_unzipped_files.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from os import listdir
 from os.path import isfile, join
 
@@ -7,20 +8,24 @@ import gzip
 import shutil
 from contextlib import ExitStack
 
-print(nib.__version__)
 
-input_folder = '/home/feczk001/shared/data/nnUNet/nnUNet_raw_data_base/nnUNet_raw_data/Task509_Paper/labelsTr'
-os.chdir(input_folder)
-only_files = [f for f in listdir(input_folder) if isfile(join(input_folder, f))]
-for f in only_files:
-    try:
-        img = nib.load(f)
-    except Exception as ex:
-        print(ex)
-        with ExitStack() as stack:
-            new_name = f[:-3]
-            os.rename(f, new_name)
-            f_in = stack.enter_context(open(new_name, 'rb'))
-            f_out = stack.enter_context(gzip.open(new_name + '.gz', 'wb'))
-            shutil.copyfileobj(f_in, f_out)
-            os.remove(new_name)
+def fix_unzipped_files(input_folder):
+    os.chdir(input_folder)
+    only_files = [f for f in listdir(input_folder) if isfile(join(input_folder, f))]
+    for f in only_files:
+        try:
+            nib.load(f)
+        except Exception as ex:
+            print(ex)
+            with ExitStack() as stack:
+                new_name = f[:-3]
+                os.rename(f, new_name)
+                # noinspection PyTypeChecker
+                f_in = stack.enter_context(open(new_name, 'rb'))
+                f_out = stack.enter_context(gzip.open(new_name + '.gz', 'wb'))
+                shutil.copyfileobj(f_in, f_out)
+                os.remove(new_name)
+
+
+if __name__ == "__main__":
+    fix_unzipped_files(sys.argv[1])
